57d94780e8673e5be57271a23d662adb63d036af
Fareez Ahamed <fareez.ahamed@gmail.com>
2014-02-18 14:52:53 +0530

    Created users table

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'openui5',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
